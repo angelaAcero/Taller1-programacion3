0e8b7f6addf252f3e758e7a0102b50e819500def
angelaAcero <158375313+angelaAcero@users.noreply.github.com>
2024-02-06 17:42:35 -0500

    Add files via upload

---
+++ lab1punto1/Lab1punto1.java
@@ -0,0 +1,12 @@
+
+package lab1punto1;
+
+
+public class Lab1punto1 {
+
+  
+    public static void main(String[] args) {
+   System.out.println("Hola Mundo, Angela Maria Acero Hernandez, 1804452");
+    }
+    
+}
